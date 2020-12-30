a22952c7d6a7dbf23f12c9a297de672f72aeafd0
jylee <wjddp3@naver.com>
2020-12-30 16:26:38 +0900

    Init react-native project

---
+++ index.native.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+// Entry point of native app. 
+import {AppRegistry} from 'react-native';
+import App from './src/App';
+import {name as appName} from './src/app.json';
+
+AppRegistry.registerComponent(appName, () => App);
